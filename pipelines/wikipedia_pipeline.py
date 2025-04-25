1fad0760f98dccee212b72f8b9c9e98fad6bb01c
OrestesChZ <oreschz@hotmail.com>
2025-04-25 10:46:11 -0500

    commit

--- pipelines/wikipedia_pipeline.py
+++ pipelines/wikipedia_pipeline.py
@@ -16,3 +16,14 @@ def get_wikipedia_page(url):
         return response.text
     except requests.RequestException as e:
         print(f"An error occured: {e}")
+
+########
+
+def get_wikipedia_data(html):
+    from bs4 import BeautifulSoup
+
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find_all("table", {"class" : "wikitable sortable"})[0]
+
+    table_rows = table.find_all('tr')
+    return table_rows
